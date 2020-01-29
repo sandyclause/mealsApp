d820f66820f8f4238cf41fd50e0e59db02382cf5
sandy <sandytian2000@hotmail.com>
2020-01-28 21:04:23 -0500

    fakenews data

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-
+import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+const renderGridItem = (itemData) => {
+  return <View style={styles.gridItem}>
+    <Text>{itemData.item.title}</Text>
+  </View>
+}
 const CategoriesScreen = props => {
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen</Text>
-      <Button title='go to meals' onPress={() => {
-        props.navigation.navigate({
-          routeName: 'CategoryMeals'
-        })
-      }} />
-    </View>
+    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   )
 };
-
 const styles = StyleSheet.create({
-
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+  }
 });
-
  export default CategoriesScreen;
