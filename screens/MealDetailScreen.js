0190ee533629cecbd74ecf842b48655d06e80394
sandy <sandytian2000@hotmail.com>
2020-02-02 10:30:14 -0500

    meal detail screen infos

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import { MEALS } from '../data/dummy-data';
 
 const MealDetailScreen = props => {
+  const mealId = props.navigation.getParam('mealId');
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
   return (
     <View style={styles.screen}>
-      <Text>The Meal deals Screen</Text>
+      <Text>{selectedMeal.title}</Text>
       <Button
         title='go back to categories'
         onPress={() => {
@@ -14,6 +18,15 @@ const MealDetailScreen = props => {
     </View>
   )
 };
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam('mealId');
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
+  return {
+    headerTitle: selectedMeal.title
+  }
+}
 
 const styles = StyleSheet.create({
 
