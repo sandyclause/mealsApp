6cd5457538ab2dc259a2e53d84deba0b8461788c
sandy <sandytian2000@hotmail.com>
2020-02-02 12:28:56 -0500

    header refactor

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import { MEALS } from '../data/dummy-data';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam('mealId');
@@ -24,7 +26,20 @@ MealDetailScreen.navigationOptions = (navigationData) => {
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
   return {
-    headerTitle: selectedMeal.title
+    headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons
+        HeaderButtonComponent={HeaderButton}
+      >
+        <Item
+          title='Favorite'
+          iconName='ios-star'
+          onPress={() => {
+            console.log('test')
+          }}
+        />
+      </HeaderButtons>
+    )
   }
 }
 
